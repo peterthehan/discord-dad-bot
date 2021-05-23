781357d2c55e38b325039b7c743ab3241afd4040
Peter Han <peter.han.9493@gmail.com>
2021-05-23 00:30:20 -0700

    Update random fn

--- src-discord-dad-bot/handlers/message.js
+++ src-discord-dad-bot/handlers/message.js
@@ -11,8 +11,7 @@ const getWebhook = async (message) => {
 const getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
-
-  return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min) + min);
 };
 
 module.exports = async (message) => {
